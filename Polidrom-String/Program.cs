e61551d6e4c90def7805c19e60dc03e1d7fe1926
ThugXD <vvalterzandamela@gmail.com>
2024-03-01 23:24:08 +0200

    Polidrom String

---
+++ Polidrom-String/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pallindrome
+{
+    class Palindrome
+    {
+        static void Main(string[] args)
+        {
+            String original, reverse = "";
+
+            Console.WriteLine("Enter a string : ");
+            original = Console.ReadLine();
+
+            int length = original.Length;
+
+            for (int i = length - 1; i >= 0; i--)
+                reverse = reverse + original.ElementAt(i);
+
+            if (original.Equals(reverse))
+                Console.WriteLine("Entered string is a palindrome.");
+            else
+                Console.WriteLine("Entered string is not a palindrome.");
+
+            Console.ReadLine();
+        }
+    }
+}
