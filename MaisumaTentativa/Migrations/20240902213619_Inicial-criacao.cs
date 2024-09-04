1fb6e15844d8ae84bcd49a98f5acd09d1db43736
ThugXD <vvalterzandamela@gmail.com>
2024-09-04 07:10:35 +0200

    CRUD com asp.net

---
+++ MaisumaTentativa/Migrations/20240902213619_Inicial-criacao.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MaisumaTentativa.Migrations
+{
+    /// <inheritdoc />
+    public partial class Inicialcriacao : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Produto",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Produto", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Produto");
+        }
+    }
+}
---
+++ MaisumaTentativa/Models/Produto.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MaisumaTentativa.Models
+{
+	[Table("Produto")]
+	public class Produto
+	{
+		[Column("Id")]
+		[Display(Name ="Codigo")]
+		public int Id { get; set; }
+
+		[Column("Nome")]
+		[Display(Name = "Nome")]
+		public string Nome { get; set; }
+
+		[Column("Email")]
+		[Display(Name = "Email")]
+		public string Email { get; set; }
+	}
+}
